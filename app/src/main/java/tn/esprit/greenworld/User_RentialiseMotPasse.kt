3533f883d61e6653db4bfcb83964a98d1b4e0ab1
aymenzouaoui <aymen.zouaoui@esprit.tn>
2023-11-06 00:32:32 +0100

    GestionUser_RenitialisationMotPasse_Commit

---
+++ app/src/main/java/tn/esprit/greenworld/User_RentialiseMotPasse.kt
@@ -0,0 +1,11 @@
+package tn.esprit.greenworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class User_RentialiseMotPasse : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_rentialise_mot_passe)
+    }
+}
