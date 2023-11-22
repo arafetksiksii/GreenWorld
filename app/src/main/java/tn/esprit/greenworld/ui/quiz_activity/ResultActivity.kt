ad9e0c4ba86a3bb0751e9c8acef00d265a0e3c5f
Walidbouzouita <125035094+Walidbouzouita@users.noreply.github.com>
2023-11-22 08:19:59 +0100

    pdf

--- app/src/main/java/tn/esprit/greenworld/ui/quiz_activity/ResultActivity.kt
+++ app/src/main/java/tn/esprit/greenworld/ui/quiz_activity/ResultActivity.kt
@@ -1,7 +1,11 @@
 package tn.esprit.greenworld.ui.quiz_activity
 
+import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.content.SharedPreferences
+import android.graphics.BitmapFactory
+import android.graphics.Paint
+import android.graphics.pdf.PdfDocument
 import android.media.MediaPlayer
 import android.os.Bundle
 import android.util.Log
@@ -9,6 +13,8 @@ import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
+import okio.IOException
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -18,6 +24,8 @@ import tn.esprit.greenworld.models.User4
 import tn.esprit.greenworld.ui.gestionUser.LoginActivity
 import tn.esprit.greenworld.utils.RetrofitImp
 import tn.esprit.greenworld.utils.UserInterface
+import java.io.File
+import java.io.FileOutputStream
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,42 +33,94 @@ class ResultActivity : AppCompatActivity() {
         setContentView(R.layout.result)
 
         // Initialisation du MediaPlayer pour l'effet sonore des résultats
-        var mediaPlayer = MediaPlayer.create(this, R.raw.result_congratulations)
-        mediaPlayer.start() // Jouez le son dès que l'activité démarre
+        val mediaPlayer = MediaPlayer.create(this, R.raw.result_congratulations)
+        mediaPlayer.start()
 
-        // Assurez-vous de libérer le MediaPlayer lorsque vous avez fini avec
         mediaPlayer.setOnCompletionListener { mp -> mp.release() }
 
         val score = intent.getIntExtra("SCORE", 0)
         val totalQuestions = intent.getIntExtra("TOTAL_QUESTIONS", 0)
-        val correctAnswers = intent.getIntExtra("CORRECT_ANSWERS", 0)
 
         val txtScore = findViewById<TextView>(R.id.txt_score)
         txtScore.text = "${(score.toDouble() / totalQuestions * 100).toInt()}% Score"
+
         val txtNbrQuestions = findViewById<TextView>(R.id.txt_nbr_questions)
-        val txtNbrReponseCorrecte = findViewById<TextView>(R.id.txt_nbr_reponse_correcte)
-
         txtNbrQuestions.text = "$totalQuestions"
-        txtNbrReponseCorrecte.text = "$correctAnswers"
 
-        // Get user data from SharedPreferences
+        val txtNbrReponseCorrecte = findViewById<TextView>(R.id.txt_nbr_reponse_correcte)
+        txtNbrReponseCorrecte.text = "$score"
+
         val sharedPreferences: SharedPreferences = getSharedPreferences("user_pref", MODE_PRIVATE)
         val userId = sharedPreferences.getString("userId", "")
 
-        // Make the API call to update the user's score
-        userId?.let {
-            updateScore(it, score)
-
-        }
+        userId?.let { updateScore(it, score) }
 
         val backButton = findViewById<ImageView>(R.id.img_back)
         backButton.setOnClickListener {
-            // Create an intent to start a new activity
             val intent = Intent(this, QuizListActivity::class.java)
-            // Start the activity
             startActivity(intent)
-            // Optionally: if you don't want to keep the current activity in the activity stack
             finish()
+        }
+
+        val pdfDocument = PdfDocument()
+        val pageInfo = PdfDocument.PageInfo.Builder(600, 800, 1).create()
+        val page = pdfDocument.startPage(pageInfo)
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.img_titre_quiz)
+
+
+        val canvas = page.canvas
+        val paint = Paint().apply { textSize = 16f }
+
+        // Positionnez l'image en haut du document
+        val imageX = (pageInfo.pageWidth - bitmap.width) / 2f  // Centrer horizontalement
+        val imageY = 10f  // Un peu d'espace à partir du haut
+        var y = imageY + bitmap.height + 20  // Ajoutez la hauteur de l'image plus un peu d'espace
+
+
+        canvas.drawBitmap(bitmap, imageX, imageY, null)
+
+        canvas.drawText("Résultats du Quiz", 10f, y, paint)
+        y += paint.descent() - paint.ascent()
+
+        canvas.drawText("ID de l'utilisateur: $userId", 10f, y, paint)
+        y += paint.descent() - paint.ascent()
+
+        canvas.drawText("Score: $score / $totalQuestions", 10f, y, paint)
+        pdfDocument.finishPage(page)
+
+        val filePath = File(getExternalFilesDir(null), "ResultatsQuiz.pdf")
+        try {
+            pdfDocument.writeTo(FileOutputStream(filePath))
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        pdfDocument.close()
+
+        val pdfFile = File(getExternalFilesDir(null), "ResultatsQuiz.pdf")
+        val contentUri = FileProvider.getUriForFile(this, "tn.esprit.greenworld.fileprovider", pdfFile)
+
+        val imgPdfAfficher = findViewById<ImageView>(R.id.img_pdf_afficher)
+        imgPdfAfficher.setOnClickListener {
+            try {
+                val viewIntent = Intent(Intent.ACTION_VIEW).apply {
+                    setDataAndType(contentUri, "application/pdf")
+                    addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                }
+                startActivity(viewIntent)
+            } catch (e: ActivityNotFoundException) {
+                Toast.makeText(this, "Aucune application pour ouvrir le PDF", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        val imgPdfPartager = findViewById<ImageView>(R.id.img_pdf_partager)
+        imgPdfPartager.setOnClickListener {
+            val shareIntent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_STREAM, contentUri)
+                type = "application/pdf"
+                addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+            }
+            startActivity(Intent.createChooser(shareIntent, "Partager le PDF"))
         }
     }
 
