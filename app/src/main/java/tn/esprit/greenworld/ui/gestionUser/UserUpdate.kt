5ed514897deb71cd86e04b20be3f2c5c4b09e0e7
aymenzouaoui <aymen.zouaoui@esprit.tn>
2023-12-29 22:26:55 +0100

    UpdateImage

--- app/src/main/java/tn/esprit/greenworld/ui/gestionUser/UserUpdate.kt
+++ app/src/main/java/tn/esprit/greenworld/ui/gestionUser/UserUpdate.kt
@@ -6,7 +6,6 @@ import android.content.SharedPreferences
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
-import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
@@ -22,7 +21,6 @@ import tn.esprit.greenworld.databinding.ActivityUserUpdateBinding
 import tn.esprit.greenworld.models.User
 import tn.esprit.greenworld.models.User5
 import tn.esprit.greenworld.ui.gestionUser.LoginActivity
-
 import tn.esprit.greenworld.utils.RetrofitImp
 import tn.esprit.greenworld.utils.UserInterface
 
@@ -30,38 +28,30 @@ class UserUpdate : AppCompatActivity() {
     var config: HashMap<String, String> = HashMap()
     private lateinit var binding: ActivityUserUpdateBinding
     private lateinit var selectedImageView: ImageView
-    private lateinit var sharedPreferences: SharedPreferences // Déplacer ici
-
+    private lateinit var sharedPreferences: SharedPreferences
 
     companion object {
         const val PICK_IMAGE_REQUEST = 1
     }
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityUserUpdateBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Handle Return button click
         binding.btnReturn.setOnClickListener {
             finish()
         }
-// Initialiser sharedPreferences ici
+
         sharedPreferences = getSharedPreferences("user_pref", MODE_PRIVATE)
 
-
-
-
         // Check if MediaManager is already initialized
-
-        // Initialize Cloudinary with configuration
-        config["cloud_name"] = "dznvwntjn"
-        config["api_key"] = "972319243848173"
-        config["api_secret"] = "xp2G8BXbjvjec0dbFIaQbUJ3Mj8"
-        MediaManager.init(this, config)
-
+        if (!MediaManager.get().isInitialized) {
+            config["cloud_name"] = "dznvwntjn"
+            config["api_key"] = "972319243848173"
+            config["api_secret"] = "xp2G8BXbjvjec0dbFIaQbUJ3Mj8"
+            MediaManager.init(this, config)
+        }
 
         selectedImageView = findViewById(R.id.selectedImageView)
 
@@ -72,14 +62,11 @@ class UserUpdate : AppCompatActivity() {
 
         val uploadImageButton: Button = findViewById(R.id.uploadImageButton)
         uploadImageButton.setOnClickListener {
-            // Check if an image is selected before attempting to upload
             if (selectedImageUri != null) {
                 uploadImageToCloudinary(selectedImageUri!!)
-
             } else {
                 Toast.makeText(this, "Select an image first", Toast.LENGTH_SHORT).show()
             }
-            updateUserp()
         }
     }
 
@@ -94,31 +81,21 @@ class UserUpdate : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
 
         if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK) {
-            // The image has been successfully selected
             selectedImageUri = data?.data
-
-            // Display the selected image in the ImageView
             selectedImageView.setImageURI(selectedImageUri)
         }
     }
 
     private fun uploadImageToCloudinary(imageUri: Uri) {
-
-        // Use MediaManager configuration
         MediaManager.get().upload(imageUri).callback(object : UploadCallback {
-            override fun onStart(requestId: String?) {
-                // Handle the start of the upload
-            }
+            override fun onStart(requestId: String?) {}
 
-            override fun onProgress(requestId: String?, bytes: Long, totalBytes: Long) {
-                // Handle the progress of the upload
-            }
+            override fun onProgress(requestId: String?, bytes: Long, totalBytes: Long) {}
 
             override fun onSuccess(requestId: String?, resultData: MutableMap<Any?, Any?>?) {
-                // Handle the success of the upload
                 val publicId = resultData?.get("public_id")
                 val url = MediaManager.get().url().generate(publicId.toString())
-
+                updateUserp(url)
                 Toast.makeText(
                     this@UserUpdate,
                     "Image uploaded successfully. URL: $url",
@@ -127,7 +104,6 @@ class UserUpdate : AppCompatActivity() {
             }
 
             override fun onError(requestId: String?, error: ErrorInfo?) {
-                // Handle errors during the upload
                 Toast.makeText(
                     this@UserUpdate,
                     "Error uploading the image: ${error?.description}",
@@ -135,26 +111,22 @@ class UserUpdate : AppCompatActivity() {
                 ).show()
             }
 
-            override fun onReschedule(requestId: String?, error: ErrorInfo?) {
-                // Handle the rescheduling of the upload
-            }
+            override fun onReschedule(requestId: String?, error: ErrorInfo?) {}
         }).dispatch()
     }
-    private fun updateUserp() {
-        // Get user data from SharedPreferences
-        // val sharedPreferences: SharedPreferences = getSharedPreferences("user_pref", MODE_PRIVATE)
+
+    private fun updateUserp(imageUrl: String) {
         val userId = sharedPreferences.getString("userId", "")
-        val imageUrl=sharedPreferences.getString("imageRes","")
-        Log.d("hhhhhhhhhhh","jjjjjjjjjjjjjjjj")
+
         val user5 = User5(
-            id = userId?.toString() ?: "", // Assurez-vous que userId n'est pas null
-            email = binding.tiEmail.text.toString(),
-            nom = binding.tiEmail.text.toString(), // Utilisez le champ correct pour le nom
-            prenom = binding.tiEmail.text.toString(), // Utilisez le champ correct pour le prénom
-            userName = binding.tiFullName.text.toString(), // Assurez-vous que tiFullName est le champ correct
-            adress = binding.tiAdress.text.toString(),
-            cin = binding.tiCin.text.toString(),
-            imageRes = imageUrl?.toString() ?: "" // Assurez-vous que imageUrl n'est pas null
+            id = userId?.toString() ?: "",
+            email = "",
+            nom = "",
+            prenom = "",
+            userName = "",
+            adress = "",
+            cin = "",
+            imageRes = imageUrl
         )
 
         val imageService = RetrofitImp.buildRetrofit().create(UserInterface::class.java)
@@ -168,23 +140,20 @@ class UserUpdate : AppCompatActivity() {
             }
 
             override fun onFailure(call: Call<User>, t: Throwable) {
-                // Handle the failure here
-                // You can log the error or show a message to the user
                 handleUpdateFailure(t.message ?: "Unknown error")
             }
         })
     }
+
     private fun handleSuccessfulupdate(user: User?) {
-        // Handle successful registration
-        // Optionally, you can navigate to the login screen
         startActivity(Intent(this@UserUpdate, MIDrawerActivity::class.java))
         finish()
     }
 
     private fun handleUpdateFailure(errorMessage: String) {
-        // Handle registration failure
         showSnackbar(errorMessage)
     }
+
     private fun showSnackbar(message: String) {
         Snackbar.make(binding.root, message, Snackbar.LENGTH_SHORT).show()
     }
--- app/src/main/java/tn/esprit/greenworld/utils/RetrofitImp.kt
+++ app/src/main/java/tn/esprit/greenworld/utils/RetrofitImp.kt
@@ -8,7 +8,8 @@ import tn.esprit.greenworld.API.ReservationApi
 
 object RetrofitImp {
 
-    private const val BASE_URL = "http://10.0.2.2:9090/"
+   // private const val BASE_URL = "http://10.0.2.2:9090/"
+    private const val BASE_URL = "http://192.168.1.16:9090/"
     private val dechets: Retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
